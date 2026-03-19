9f64c9ddeebd79afa99a864fe3950661d2d0eb1b
Jake Kim <jkim837@aucklanduni.ac.nz>
2026-03-19 23:41:12 +1300

    Initial commit, spring boot project setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# vape-finder
-A web app for NZ vape shoppers to search and compare products by flavour, brand and price across Auckland stores
---
+++ src/main/java/com/jake/vapefinder/VapefinderApplication.java
@@ -0,0 +1,13 @@
+package com.jake.vapefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VapefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VapefinderApplication.class, args);
+	}
+
+}
